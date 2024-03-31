f2b53e4d014895ffecc4374a957153ab5d47505a
Bly <adgehbirhane@gmail.com>
2024-03-31 22:08:20 +0300

    code refactor

--- app/screens/login/Login.jsx
+++ app/screens/login/Login.jsx
@@ -1,33 +1,19 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Text, ImageBackground, StyleSheet, TextInput, View, TouchableOpacity, Image } from 'react-native';
+import { ImageBackground, StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import LoginForm from './LoginForm';
 
 export default function Login() {
-    const navigate = useNavigation();
+    const dismissKeyboard = () => {
+        Keyboard.dismiss();
+    };
     return (
-        <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={dismissKeyboard} style={styles.container}>
             <ImageBackground style={styles.illustration} source={require("../../../assets/login.png")}>
                 <View style={styles.header}>
                 </View>
-                <View style={styles.form}>
-                    <TextInput style={styles.textField} placeholder='Enter username' required />
-                    <TextInput style={styles.textField} placeholder='Enter password' textContentType='password' />
-                    <TouchableOpacity style={styles.button} onPress={() => navigate.push('Menu')} >
-                        <View>
-                            <Text style={styles.buttonText}>LOG IN</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, styles.google_btn]} onPress={() => {/* Handle Google sign-in */ }}>
-                        <View>
-                            <Text style={styles.buttonText}>SIGN IN WITH GOOGLE</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => navigate.push('SignUp')}>
-                        <Text style={styles.text}>Don't have an account? <Text style={styles.link}>Sign Up</Text></Text>
-                    </TouchableOpacity>
-                </View>
+                <LoginForm />
             </ImageBackground>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -40,52 +26,8 @@ const styles = StyleSheet.create({
         paddingLeft: "10%",
         paddingRight: "10%",
     },
-    form: {
-        flex: 1.5,
-        justifyContent: 'center',
-        justifyItems: 'center',
-        paddingLeft: "10%",
-        paddingRight: "5%",
-        gap: 12,
-    },
     illustration: {
         flex: 1,
         resizeMode: 'contain',
     },
-    image: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    textField: {
-        borderWidth: 0.2,
-        borderRadius: 5,
-        borderColor: 'gray',
-        fontSize: 16,
-        padding: 7,
-        paddingLeft: 20,
-        backgroundColor: '#e4eaeb'
-    },
-    button: {
-        borderRadius: 5,
-        padding: 7,
-        borderWidth: 0.2,
-        borderRadius: 5,
-        backgroundColor: '#2888eb',
-    },
-    google_btn: {
-        backgroundColor: 'darkgray',
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-        textAlign: 'center',
-    },
-    text: {
-        color: 'black',
-        textAlign: 'center'
-    },
-    link: {
-        color: 'blue'
-    }
-
 });
--- app/screens/login/LoginForm.jsx
+++ app/screens/login/LoginForm.jsx
@@ -1,34 +1,115 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, TextInput, View, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; // Import icons from Expo vector icons
 
 export default function LoginForm() {
+    const navigate = useNavigation();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <View style={styles.login}>
-            <Text style={styles.title}>Login</Text>
-            {/* <View style={styles.inputs}>
-                <TextInput style={styles.textField} placeholder='username' />
-                <TextInput style={styles.textField} placeholder='password' />
-                <Button style={styles.button} title='SIGN IN' />
-            </View> */}
+        <View style={styles.form}>
+            <View style={styles.inputContainer}>
+                <MaterialCommunityIcons name="account" size={24} color="black" style={styles.icon} />
+                <TextInput style={styles.textField} placeholder='Enter username' required />
+            </View>
+            <View style={styles.inputContainer}>
+                <MaterialCommunityIcons name="lock" size={24} color="black" style={styles.icon} />
+                <TextInput
+                    style={styles.textField}
+                    placeholder='Enter password'
+                    secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity onPress={toggleShowPassword} style={styles.eyeIcon}>
+                    <MaterialCommunityIcons
+                        name={showPassword ? "eye-off" : "eye"}
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={() => navigate.push('Menu')} >
+                <View>
+                    <Text style={styles.buttonText}>LOG IN</Text>
+                </View>
+            </TouchableOpacity>
+            <View>
+                <Text style={styles.text}>Or</Text>
+            </View>
+            <TouchableOpacity style={[styles.button, styles.google_btn]} onPress={() => {/* Handle Google sign-in */ }}>
+                <View>
+                    <Text style={styles.buttonText}>SIGN IN WITH GOOGLE</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigate.push('SignUp')}>
+                <Text style={styles.text}>Don't have an account?
+                    <Text style={styles.link}>Sign Up</Text>
+                </Text>
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
 
-
 const styles = StyleSheet.create({
-    login: {
-        flex: 1,
-    },
-    image: {
-        flex: 1,
+    form: {
+        flex: 1.5,
         justifyContent: 'center',
+        justifyItems: 'center',
+        paddingLeft: "10%",
+        paddingRight: "5%",
+        gap: 12,
     },
-    title: {
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    icon: {
+        marginLeft: 10,
+    },
+    textField: {
+        flex: 1,
+        borderWidth: 0.2,
+        borderRadius: 5,
+        borderColor: 'gray',
+        fontSize: 16,
+        padding: 7,
+        paddingLeft: 20,
+        backgroundColor: '#e4eaeb'
+    },
+    eyeIcon: {
+        position: 'absolute',
+        right: 6,
+        top: '50%',
+        paddingHorizontal: 5,
+        transform: [{ translateY: -10 }],
+        backgroundColor: '#e4eaeb'
+    },
+    button: {
+        borderRadius: 5,
+        padding: 7,
+        borderWidth: 0.2,
+        borderRadius: 5,
+        backgroundColor: '#2888eb',
+    },
+    google_btn: {
+        backgroundColor: 'darkgray',
+    },
+    buttonText: {
         color: 'white',
-        fontSize: 42,
-        lineHeight: 84,
-        fontWeight: 'bold',
+        fontSize: 16,
         textAlign: 'center',
-        backgroundColor: '#000000c0',
     },
+    text: {
+        color: 'black',
+        textAlign: 'center'
+    },
+    link: {
+        color: 'blue',
+        paddingTop: 5,
+    }
+
 });
