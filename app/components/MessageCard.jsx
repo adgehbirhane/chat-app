59d0e01cd2550e1062a1e7bf8894ae60ecd3ad73
adgehbirhane <adgehbirhane@gmail.com>
2024-04-02 10:32:13 +0300

    avatar added for message card

--- app/components/MessageCard.jsx
+++ app/components/MessageCard.jsx
@@ -1,19 +1,35 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Avatar } from "react-native-paper"; 
 
-import { globalStyles } from '../utils/globalStyles'
+import { globalStyles } from '../utils/globalStyles';
 
 export default function MessageCard({ oneSMS }) {
     return (
         <View>
             <Text style={{ textAlign: 'center' }}>{oneSMS.createdAt}</Text>
-
-            {/* <Avatar /> */}
-            <View style={[globalStyles.card, oneSMS.sender == "mine" ? globalStyles.myMessage : globalStyles.othersMessage]}>
-                <Text>{oneSMS.name}</Text>
-                <Text>{oneSMS.message}</Text>
-            </View>
-            {/* <Avatar /> */}
+            <View style={{alignSelf:  oneSMS.sender == "other" ? "flex-start": "flex-end",  flexDirection: 'row', marginHorizontal: 10}} >
+            <Avatar.Image 
+            style={{  display: oneSMS.sender == "other" ? "block": "none"}}
+                            size={50} 
+                            source={{ 
+                                uri:  
+                                     `https://media.geeksforgeeks.org/wp-content/uploads/20220305133853/gfglogo-300x300.png`, 
+                            }} 
+                        /> 
+                        <View style={[globalStyles.card, oneSMS.sender == "mine" ? globalStyles.myMessage : globalStyles.othersMessage]}>
+                            <Text>{oneSMS.name}</Text>
+                            <Text>{oneSMS.message}</Text>
+                        </View>
+                        <Avatar.Image 
+                            style={{ display: oneSMS.sender == "mine" ? "block" : "none" }} 
+                            size={50} 
+                            source={{ 
+                                uri:  
+                                     `https://media.geeksforgeeks.org/wp-content/uploads/20220305133853/gfglogo-300x300.png`, 
+                            }} 
+                        /> 
+            </View> 
         </View>
     )
 }
